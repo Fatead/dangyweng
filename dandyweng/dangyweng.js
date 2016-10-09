7cf0c69d3c7ae68d3a89f9760794cfea1b6ef4ca
fatead <893051689@qq.com>
2016-10-09 15:20:32 +0800

    changes some

--- dandyweng/dangyweng.js
+++ dandyweng/dangyweng.js
@@ -221,59 +221,166 @@ window.onload = function(){
     {
         oTop.style.opacity = 0;
     };
-    document.getElementById("svg32").onmouseenter = function()
-    {
-        oTop.innerHTML = "沙发沙发";
-    };
-
-}
+};
 
 var set= 0;
+var set2= 0;
+var set3 = 0;
 var i= 0;
 var j = 0;
+var m = 0;
+var n = 0;
+var r = 0;
+var e = 0;
 var home2 = document.getElementById( "heading" );
 var home3 = document.getElementById( "home-p" );
-var intervalId;
-var intervalId2;
 var sign;
 
 
 function outCon()
 {
+    var intervalId2 = null;
     var home2 = document.getElementById( "heading" );
     var home3 = document.getElementById( "home-p" );
-    home2.style.top = 120 + j*5 +"px";
-    home3.style.top = 260 + j*5 +"px" ;
-    console.log("2333");
-    if(j<170)
-    {
-        j++;
-    }
+    intervalId2 = setInterval(function () {
+        home2.style.top = 120 + j*5 +"px";
+        home3.style.top = 260 + j*5 +"px" ;
+        if(j<170)
+        {
+            j++;
+        }
 
-    else
-    {
-        set=0;
-        clearInterval(intervalId2);
-    }
+        else
+        {
+            set=0;
+            clearInterval(intervalId2);
+            i=0;
+        }
+    }, 5);
 }
 
-function showCon()
+function showCon() {
+    var timeline = document.getElementById("cd-timeline");
+    var home2 = document.getElementById("heading");
+    var home3 = document.getElementById("home-p");
+    var intervalId = null;
+    intervalId = setInterval(function () {
+        home2.style.top = 970 - i * 5 + "px";
+        home3.style.top = 1110 - i * 5 + "px";
+        if (i < 170) {
+            i++;
+        }
+        else {
+            set = 1;
+            clearInterval(intervalId);
+            j=0;
+        }
+    }, 5);
+    timeline.style.opacity = 1;
+    home2.style.opacity = 1;
+    home3.style.opacity = 1;
+}
+
+function showTravel() {
+    var travel1 = document.getElementById("travel-section1");
+    var travel2 = document.getElementById("travel-section2");
+    travel2.style.display = "block";
+    var intervalId3 = null;
+    intervalId3 = setInterval(function () {
+        travel1.style.left = -850 + m * 5 + "px";
+        travel2.style.left = 1780 - m * 6 + "px";
+        if ( m < 170) {
+            m++;
+        }
+        else {
+            set2 = 1;
+            clearInterval(intervalId3);
+            n=0;
+        }
+    }, 5);
+}
+
+function outTravel() {
+    var travel1 = document.getElementById("travel-section1");
+    var travel2 = document.getElementById("travel-section2");
+    var intervalId4 = null;
+    travel2.style.display = "none";
+    intervalId4 = setInterval(function () {
+        travel1.style.left = 0 - n * 5 + "px";
+        travel2.style.left = 760 + n * 6 + "px";
+        if ( n < 170) {
+            n++;
+        }
+        else {
+            set2 = 0;
+            clearInterval(intervalId4);
+            m=0;
+        }
+    }, 5);
+}
+
+function showDesign() {
+    var design1 = document.getElementById("design-section1");
+    var design2 = document.getElementById("design-section2");
+    design1.style.display = "block";
+    design2.style.display = "block";
+    var intervalId5 = null;
+    intervalId5 = setInterval(function () {
+        design1.style.left = -850 + r * 5 + "px";
+        design2.style.left = 1780 - r * 6 + "px";
+        if ( r < 170) {
+            r++;
+        }
+        else {
+            set3 = 1;
+            clearInterval(intervalId5);
+            e=0;
+        }
+    }, 5);
+}
+
+function outDesign() {
+    var design1 = document.getElementById("design-section1");
+    var design2 = document.getElementById("design-section2");
+    design1.style.display = "none";
+    design2.style.display = "none";
+    var intervalId4 = null;
+    intervalId4 = setInterval(function () {
+        design1.style.left = 0 - e * 5 + "px";
+        design2.style.left = 760 + e * 6 + "px";
+        if ( e < 170) {
+            e++;
+        }
+        else
+        {
+            set3 = 0;
+            clearInterval(intervalId4);
+            r=0;
+        }
+    }, 5);
+}
+
+function showGif()
 {
-    var home2 = document.getElementById( "heading" );
-    var home3 = document.getElementById( "home-p" );
-    home2.style.top = 970 - i*5 + "px";
-    home3.style.top = 1110 - i*5 + "px" ;
-    console.log("logo");
-    if(i<170)
+    var photo = document.getElementsByClassName("photo-section5");
+    for(var f=0,len=photo.length;f<len;f++)
     {
-        i++;
+        photo[f].style.opacity = 1;
+        photo[f].style.width = 180 + "px";
     }
-    else
-    {
-        set=1;
-        clearInterval(intervalId);
-    }
+
 }
+function outGif()
+{
+    var photo = document.getElementsByClassName("photo-section5");
+    for(var f=0,len=photo.length;f<len;f++)
+    {
+        photo[f].style.opacity = 0;
+        photo[f].style.width = 20 + "px";
+    }
+
+}
+
 var time=766;
 var timer = null;
 
@@ -282,45 +389,44 @@ window.onscroll = function()
 {
     var timeline = document.getElementById( "cd-timeline" );
     var t = document.documentElement.scrollTop || document.body.scrollTop;
-    console.log(t);
+
     if( t >= 500 && t<=1500 && set==0 )
     {
-         intervalId = setInterval("showCon()", 5);
         timeline.style.opacity = 1;
-        home2.style.opacity = 1;
-        home3.style.opacity = 1;
+        showCon();
+
     }
        else if( (t<550&&set==1) || (t>1500&&set==1) )
     {
         timeline.style.opacity=0;
-        if(t>1500)
-        {
-            intervalId2 = setInterval("outCon()", 5);
-
-        }
+        outCon();
     }
-   // console.log(t);
-    if( t >= 2450 && t<=3100 )
+     else if( t >= 1500 && t<=2600 && set2==0)
     {
-
+        showTravel();
     }
-    if(t >= 500 && t<=1500)
+    else if( t < 1500 || t>2600 && set2==1)
     {
-         timeline.style.opacity = 1;
-     }
-
+        outTravel();
+    }
+    else if( t >= 2400 && t<=3500)
+    {
+        showGif();
+    }
+    else if( t < 2400 || t>3500)
+    {
+        outGif();
+    }
+    else if( t > 3300 && t< 4600 && set3==0 )
+    {
+        showDesign();
+    }
+    else if( (t < 2400 || t>3500) && set3==1)
+    {
+        outDesign();
+    }
 };
 
-/*window.onload = function()
-{
-
-    var t = document.documentElement.scrollTop || document.body.scrollTop;
-
-    if(t>=6860)
-    {
-        begin();
-    }
-};*/
 
 function begin()
 {
@@ -341,9 +447,9 @@ function progress(){
 
 function showPhoto()
 {
-    document.getElementById("photo-section").style.opacity = 1;
-    document.getElementById("left").style.opacity = 1;
-    document.getElementById("right").style.opacity = 1;
+    document.getElementById("photo-section").style.display = "block";
+    document.getElementById("left").style.display = "block";
+    document.getElementById("right").style.display = "block";
 }
 var f = 1;
 function leftMove()
@@ -363,7 +469,7 @@ function rightMove()
 
 function cancelMove()
 {
-    document.getElementById("photo-section").style.opacity = 0;
+    document.getElementById("photo-section").style.display = "none";
 
 }
 
